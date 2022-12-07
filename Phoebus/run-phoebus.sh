0352acd5d45738b70dd43fbd3cfa620a8f4e24ca
Pierrick Hanlet <hanlet@fnal.gov>
2022-12-07 11:08:05 -0600

    Modified to use new version of Phoebus: V4.7.2

--- Phoebus/run-phoebus.sh
+++ Phoebus/run-phoebus.sh
@@ -4,13 +4,13 @@
 TOP="/usr/local"
 EPICS_CONFIG="/usr/local/epics/Config"
 
-#export JAVA_HOME=$TOP/lib/jvm/jdk-11.0.2
 export JAVA_HOME=/usr/local/jdk-13.0.2
 export PATH="$JAVA_HOME/bin:$PATH"
 export EPICS_CA_ADDR_LIST="${EPICS_CA_ADDR_LIST} 131.225.120.160:6721 131.225.120.160:6722 131.225.120.160:6723"
 
 echo $TOP
-V="4.6.6-SNAPSHOT"
+#V="4.6.6-SNAPSHOT"
+V="4.7.2-SNAPSHOT"
 
 # figure out the path to the product jar
 if [[ -z "${PHOEBUS_JAR}" ]]; then
