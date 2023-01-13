77d2df827fa1168471074d4c15b608f25cc5ffb8
Pierrick Hanlet <hanlet@fnal.gov>
2023-01-13 17:13:03 -0600

    - moved Alarms - fixed directory structure in pip2it.memento

--- Alarms/startAlarmServer.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ALARMS=/usr/local/phoebus-fnal/lib/phoebus/services/alarm-server
-
-cd $ALARMS
-./alarm-server.sh -config CMTF -server acsys-services.fnal.gov:9092 -import ~/epicsDEV/Config/CSS/Phoebus/Alarms/cmtf.xml -settings /usr/local/epics/Config/CSS/Phoebus/settings.ini
-
-./alarm-server-cmtf.sh
